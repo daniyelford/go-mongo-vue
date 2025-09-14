71026bad1998c03593569e7f605e7551e73bccd0
daniyelford <29danialfrd69@gmail.com>
2025-09-14 17:30:02 +0330

    ok:)

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,12 +1,3 @@
 <template>
-  <div id="app">
     <router-view />
-  </div>
 </template>
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
